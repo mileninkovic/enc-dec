cbbc549fe4c9e9df69a91b0b7dce8ceccd974118
MINI <mini@iba.dk>
2020-10-06 16:54:38 +0200

    Removed comments and other garbage text.

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
-//let bigInt = 9007199254740991n;
-//let smallerInt = 9007199254740991n;
 let plaintext = "https://developer.mozilla.org/";
 var splitString = plaintext.split("");
-//console.log(bigInt-smallerInt);
-//console.log(res[3]);
 let encString = "";
 let decString = "";
-
-
-//console.log(encString);
 
 let encode = () => {
     splitString.forEach(char => {
@@ -19,11 +12,9 @@ let encode = () => {
     return  console.log(encoded = Buffer.from(encString).toString('hex'));
 }
 
-//url = "kwwsv=22ghyhorshu1pr}lood1ruj2";
-//var splitString = url.split("");
-
 let decode = () => {
     let decoded = Buffer.from(encoded, 'hex').toString();
+    console.log(decoded);
     let splitString = decoded.split("");
     splitString.forEach(char => {
         let val = char.charCodeAt()-3;
