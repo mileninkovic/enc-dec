47e269b8ad7775ad0f140962b48b523fc8ca3860
MINI <mini@iba.dk>
2020-10-06 16:51:35 +0200

    Added hex to encryption.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //let bigInt = 9007199254740991n;
 //let smallerInt = 9007199254740991n;
-let url = "https://developer.mozilla.org/";
-var splitString = url.split("");
+let plaintext = "https://developer.mozilla.org/";
+var splitString = plaintext.split("");
 //console.log(bigInt-smallerInt);
 //console.log(res[3]);
 let encString = "";
@@ -16,13 +16,15 @@ let encode = () => {
         let code = String.fromCharCode(val);
         encString += code;
     });
-    return console.log(encString);
+    return  console.log(encoded = Buffer.from(encString).toString('hex'));
 }
 
-url = "kwwsv=22ghyhorshu1pr}lood1ruj2";
-var splitString = url.split("");
+//url = "kwwsv=22ghyhorshu1pr}lood1ruj2";
+//var splitString = url.split("");
 
 let decode = () => {
+    let decoded = Buffer.from(encoded, 'hex').toString();
+    let splitString = decoded.split("");
     splitString.forEach(char => {
         let val = char.charCodeAt()-3;
         let code = String.fromCharCode(val);
