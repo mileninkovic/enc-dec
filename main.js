67410d64551a4ceed5dcaf4a2dfbedbd82a6836d
MINI <mini@iba.dk>
2021-09-21 08:28:21 +0200

    Added fix for spaces as character

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
-let plaintext = "https://developer.mozilla.org/";
+let plaintext = "Hallo, hvad er dit navn?";
 var splitString = plaintext.split("");
 let encString = "";
 let decString = "";
+let spaceArr = [];
+let randomSymbol = ["!", "#", "%", "&", "/", "(", ")", "=", "?"];
 
 let encode = () => {
-    splitString.forEach(char => {
+    splitString.forEach((char, index) => {
         let val = char.charCodeAt()+3;
         let code = String.fromCharCode(val);
-        encString += code;
+        if (code === "#") {
+            console.log("Virker!");
+            spaceArr.push(index);
+            console.log(spaceArr);
+            encString += randomSymbol[Math.floor(Math.random() * 9)];
+
+        } else {
+            encString += code;
+        }
     });
+    encString += " " + spaceArr;
     return  console.log(encoded = Buffer.from(encString).toString('hex'));
 }
 
@@ -16,10 +27,17 @@ let decode = () => {
     let decoded = Buffer.from(encoded, 'hex').toString();
     console.log(decoded);
     let splitString = decoded.split("");
-    splitString.forEach(char => {
+    let splitArray = decoded.split(/(\s)/);
+    let splitDecode = splitArray[2].split(",");
+    let decodable = splitArray[0].split("");
+    decodable.forEach((char, index ) => {
         let val = char.charCodeAt()-3;
         let code = String.fromCharCode(val);
-        decString += code;
+        if (splitDecode.indexOf(index.toString()) !== -1) {
+            decString += " ";
+        } else {
+            decString += code;
+        }
     });
     return console.log(decString);
 }
